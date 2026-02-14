340c510b6d4b61f4b2df5c1d5e48262b446b3145
Nourhan Osama <nourhan.o.eladwy@gmail.com>
2026-02-14 02:55:16 +0200

    Update deployOnK8s.groovy

--- vars/deployOnK8s.groovy
+++ vars/deployOnK8s.groovy
@@ -1,18 +1,23 @@
 def call(String imageName, String imageTag) {
-    echo "جاري النشر على Kubernetes باستخدام ملف الإعدادات السري... ☸️"
+    echo "جاري محاولة النشر مع تجاوز قيود الشبكة... ☸️"
     
-    // استدعاء ملف الـ kubeconfig الذي قمتِ برفعه بـ ID: 'kubeconfig-file'
     withCredentials([file(credentialsId: 'kubeconfig-file', variable: 'KUBECONFIG_PATH')]) {
         sh """
-            # 1. تحميل أداة kubectl (لضمان وجودها وصلاحيتها)
-            curl -LO "https://dl.k8s.io/release/\$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-            chmod +x ./kubectl
+            # 1. إخبار النظام بتجاهل البروكسي لهذا العنوان
+            export no_proxy=192.168.49.2,localhost,127.0.0.1
+            export NO_PROXY=192.168.49.2,localhost,127.0.0.1
+
+            # 2. تحميل kubectl (إذا لم يكن موجوداً)
+            if [ ! -f ./kubectl ]; then
+                curl -LO "https://dl.k8s.io/release/\$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+                chmod +x ./kubectl
+            fi
             
-            # 2. تحديث التاج (Tag) في ملف الـ YAML
+            # 3. تحديث ملف الـ YAML
             sed -i 's|image: .*|image: ${imageName}:${imageTag}|g' k8s/deployment.yaml
             
-            # 3. تنفيذ الأمر باستخدام ملف الإعدادات الممرر من جينكينز
-            ./kubectl apply -f k8s/deployment.yaml --kubeconfig=\$KUBECONFIG_PATH
+            # 4. محاولة النشر مع إضافة flag لتجاوز فحص الشهادات مؤقتاً للتجربة
+            ./kubectl apply -f k8s/deployment.yaml --kubeconfig=\$KUBECONFIG_PATH --insecure-skip-tls-verify
         """
     }
 }
