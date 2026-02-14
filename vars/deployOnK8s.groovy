dc956fe1e568473844c48875a6ba9bcd86664b36
Nourhan Osama <nourhan.o.eladwy@gmail.com>
2026-02-14 03:30:39 +0200

    Update deployOnK8s.groovy

--- vars/deployOnK8s.groovy
+++ vars/deployOnK8s.groovy
@@ -1,17 +1,15 @@
 def call(String imageName, String imageTag) {
-    echo "جاري النشر عبر المنفذ الموجه 32771... ☸️"
+    echo "جاري النشر مباشرة إلى Minikube عبر الشبكة المفتوحة... ☸️"
     
     withCredentials([file(credentialsId: 'kubeconfig-file', variable: 'KUBECONFIG_PATH')]) {
         sh """
             # 1. تحديث الصورة في ملف الـ Deployment
             sed -i "s|image: .*|image: ${imageName}:${imageTag}|g" k8s/deployment.yaml
             
-            # 2. النشر باستخدام العنوان المحلي والمنفذ الصحيح
+            # 2. النشر المباشر
             ./kubectl apply -f k8s/deployment.yaml \
                 --kubeconfig=\$KUBECONFIG_PATH \
-                --server=https://127.0.0.1:32771 \
-                --insecure-skip-tls-verify \
-                --validate=false
+                --insecure-skip-tls-verify
         """
     }
 }
