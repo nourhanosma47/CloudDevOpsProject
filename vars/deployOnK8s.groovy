c321743555c81926247f490d52977800606470dc
Nourhan Osama <nourhan.o.eladwy@gmail.com>
2026-02-14 03:31:40 +0200

    Update deployOnK8s.groovy

--- vars/deployOnK8s.groovy
+++ vars/deployOnK8s.groovy
@@ -1,15 +1,17 @@
 def call(String imageName, String imageTag) {
-    echo "جاري النشر مباشرة إلى Minikube عبر الشبكة المفتوحة... ☸️"
+    echo "جاري النشر المباشر... ☸️"
     
     withCredentials([file(credentialsId: 'kubeconfig-file', variable: 'KUBECONFIG_PATH')]) {
         sh """
-            # 1. تحديث الصورة في ملف الـ Deployment
+            # 1. تحديث الصورة
             sed -i "s|image: .*|image: ${imageName}:${imageTag}|g" k8s/deployment.yaml
             
-            # 2. النشر المباشر
+            # 2. النشر مع تعطيل التحقق لضمان تجاوز مشكلة المسار
             ./kubectl apply -f k8s/deployment.yaml \
                 --kubeconfig=\$KUBECONFIG_PATH \
-                --insecure-skip-tls-verify
+                --server=https://192.168.49.2:8443 \
+                --insecure-skip-tls-verify \
+                --validate=false
         """
     }
 }
