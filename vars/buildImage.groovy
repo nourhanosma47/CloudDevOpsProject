d595f1f1be8a3ad7c600a4c77e311918465aa08b
Nourhan Osama <nourhan.o.eladwy@gmail.com>
2026-02-14 02:05:06 +0200

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,4 +1,5 @@
 def call(String imageName, String imageTag) {
     echo "جاري بناء صورة Docker باسم: ${imageName}:${imageTag} 🐳"
+    sh "ls -lah" // هذا الأمر سيطبع لنا قائمة بجميع الملفات التي يراها جينكينز الآن
     sh "docker build -t ${imageName}:${imageTag} ."
 }
