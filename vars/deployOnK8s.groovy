a944a0d931be520ce068ef7024effe3f0763cf5b
Nourhan Osama <nourhan.o.eladwy@gmail.com>
2026-02-14 02:36:30 +0200

    Update deployOnK8s.groovy

--- vars/deployOnK8s.groovy
+++ vars/deployOnK8s.groovy
@@ -1,11 +1,15 @@
 def call(String imageName, String imageTag) {
-    echo "جاري البدء في عملية النشر على Kubernetes... ☸️"
+    echo "جاري تجهيز kubectl والنشر على Kubernetes... ☸️"
     
     sh """
-        # تحديث الصورة في ملف الـ YAML
+        # 1. تحميل نسخة kubectl متوافقة داخل المجلد الحالي
+        curl -LO "https://dl.k8s.io/release/\$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+        chmod +x ./kubectl
+        
+        # 2. تحديث الصورة في ملف الـ YAML
         sed -i 's|image: .*|image: ${imageName}:${imageTag}|g' k8s/deployment.yaml
         
-        # تنفيذ الأمر باستخدام المسار الكامل
-        /usr/local/bin/kubectl apply -f k8s/deployment.yaml
+        # 3. استخدام النسخة المحلية للتنفيذ
+        ./kubectl apply -f k8s/deployment.yaml --kubeconfig=\$HOME/.kube/config
     """
 }
